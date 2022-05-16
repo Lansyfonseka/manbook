f90ec99bf4aed7ecc71480efaf217029499b3f5a
Lansyfonseka <twinkstriker@gmail.com>
2022-05-16 17:04:23 +0300

    fix: style photo card

--- src/components/CollectionBook/CollectionBook.vue
+++ src/components/CollectionBook/CollectionBook.vue
@@ -1,22 +1,18 @@
 <template>
-  <div :class="{book: true, active: isOpen}" @click="isOpen = !isOpen">
-      <div className="front">
-        <div className="coverFront">
-          <div className="book__name">My collection</div>
-          <img className="book_photo" alt="book_photo" src='http://klarify.tech/wp-content/uploads/2021/04/collections.jpg'/>
+  <div :class="{book: true, active: isOpen}" @click="isOpen = !isOpen">      
+      <div className="book__front">
+        <div className="book__front_cover">
+          <div className="collection-title">My collection</div>
+          <img className="collection-photo" alt="book_photo" src='https://d14zyouj9r0hpo.cloudfront.net/wp-content/uploads/2020/12/LotsofButtonsCollectionsKids-1.jpg'/>
         </div>
-        <div className="coverBack"></div>
+        <div className="book__front_back"></div>
       </div>
-			<div className="left-side">
+			<div className="book__left-side">
         User name Author
+      </div>      
+      <div class="collection-description">
+        Desctription
       </div>
-      <!-- <PhotoCard 
-        user='Tay Loo'
-        userPhoto='https://pbs.twimg.com/media/D8dDZukXUAAXLdY.jpg'
-        item='https://images.unsplash.com/photo-1494232410401-ad00d5433cfa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-        likes={5}
-        comments={2}
-      ></PhotoCard> -->
 		</div>
 </template>
 
